7ee27e197c8eb393052d897ce079f8114100046b
itsme-brijeshrai <brijeshrai9820@gmail.com>
2025-05-03 23:12:57 +0530

    initial commit

---
+++ src/main/java/com/wos/WosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WosBackendApplication.class, args);
+	}
+
+}
